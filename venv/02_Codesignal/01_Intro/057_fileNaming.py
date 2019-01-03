af8c50502dd68d31f61a6019931b8f6a03135353
Vestenar <vestenar@gmail.com>
2019-01-04 01:01:26 +0200

    first commit1

--- venv/02_Codesignal/01_Intro/057_fileNaming.py
+++ venv/02_Codesignal/01_Intro/057_fileNaming.py
@@ -12,58 +12,57 @@ def fileNaming(names):
 
 
 assert fileNaming(["doc",
- "doc",
- "image",
- "doc(1)",
- "doc"]) == ["doc",
-         "doc(1)",
-         "image",
-         "doc(1)(1)",
-         "doc(2)"], "1"
+                   "doc",
+                   "image",
+                   "doc(1)",
+                   "doc"]) == ["doc",
+                               "doc(1)",
+                               "image",
+                               "doc(1)(1)",
+                               "doc(2)"], "1"
 
 assert fileNaming(["a(1)",
- "a(6)",
- "a",
- "a",
- "a",
- "a",
- "a",
- "a",
- "a",
- "a",
- "a",
- "a"]) == ["a(1)",
-         "a(6)",
-         "a",
-         "a(2)",
-         "a(3)",
-         "a(4)",
-         "a(5)",
-         "a(7)",
-         "a(8)",
-         "a(9)",
-         "a(10)",
-         "a(11)"], "2"
+                   "a(6)",
+                   "a",
+                   "a",
+                   "a",
+                   "a",
+                   "a",
+                   "a",
+                   "a",
+                   "a",
+                   "a",
+                   "a"]) == ["a(1)",
+                             "a(6)",
+                             "a",
+                             "a(2)",
+                             "a(3)",
+                             "a(4)",
+                             "a(5)",
+                             "a(7)",
+                             "a(8)",
+                             "a(9)",
+                             "a(10)",
+                             "a(11)"], "2"
 
 assert fileNaming(["dd",
- "dd(1)",
- "dd(2)",
- "dd",
- "dd(1)",
- "dd(1)(2)",
- "dd(1)(1)",
- "dd",
- "dd(1)"]) == ["dd",
-             "dd(1)",
-             "dd(2)",
-             "dd(3)",
-             "dd(1)(1)",
-             "dd(1)(2)",
-             "dd(1)(1)(1)",
-             "dd(4)",
-             "dd(1)(3)"], "3"
+                   "dd(1)",
+                   "dd(2)",
+                   "dd",
+                   "dd(1)",
+                   "dd(1)(2)",
+                   "dd(1)(1)",
+                   "dd",
+                   "dd(1)"]) == ["dd",
+                                 "dd(1)",
+                                 "dd(2)",
+                                 "dd(3)",
+                                 "dd(1)(1)",
+                                 "dd(1)(2)",
+                                 "dd(1)(1)(1)",
+                                 "dd(4)",
+                                 "dd(1)(3)"], "3"
 print("well done")
-
 
 '''
 You are given an array of desired filenames in the order of their creation. 
@@ -76,4 +75,5 @@ Return an array of names that will be given to the files.
 Example
 
 For names = ["doc", "doc", "image", "doc(1)", "doc"], the output should be
-fileNaming(names) = ["doc", "doc(1)", "image", "doc(1)(1)", "doc(2)"].'''
+fileNaming(names) = ["doc", "doc(1)", "image", "doc(1)(1)", "doc(2)"].
+'''
