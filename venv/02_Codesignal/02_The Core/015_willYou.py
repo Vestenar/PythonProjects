96c851372611711c874f60f971f2247305d75cbf
Vestenar <vestenar@gmail.com>
2019-01-08 23:50:16 +0200

    Th

---
+++ venv/02_Codesignal/02_The Core/015_willYou.py
@@ -0,0 +1,40 @@
+def willYou(young, beautiful, loved):
+    if loved == False and young == True and beautiful == True:
+        return True
+    elif loved == True and young == True and beautiful == True:
+        return False
+    else:
+        return loved
+
+
+def willYou2(young, beautiful, loved):
+    return loved != (young and beautiful)
+
+print(willYou(False, False, False))
+print(willYou2(False, False, False))
+
+'''
+Once Mary heard a famous song, and a line from it stuck in her head.
+That line was "Will you still love me when I'm no longer young and beautiful?".
+Mary believes that a person is loved if and only if he/she is both young and beautiful,
+but this is quite a depressing thought, so she wants to put her belief to the test.
+
+Knowing whether a person is young, beautiful and loved, find out if they contradict Mary's belief.
+
+A person contradicts Mary's belief if one of the following statements is true:
+
+    they are young and beautiful but not loved;
+    they are loved but not young or not beautiful.
+
+Example
+
+    For young = true, beautiful = true, and loved = true, the output should be
+    willYou(young, beautiful, loved) = false.
+
+    Young and beautiful people are loved according to Mary's belief.
+
+    For young = true, beautiful = false, and loved = true, the output should be
+    willYou(young, beautiful, loved) = true.
+
+    Mary doesn't believe that not beautiful people can be loved.
+'''
