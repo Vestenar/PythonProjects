374d16e6b41e639f88cafc20dbeaf23161f91f34
Vestenar <vestenar@gmail.com>
2019-01-16 00:44:11 +0200

    Core part5 completed

---
+++ venv/02_Codesignal/02_The Core/035_createArray.py
@@ -0,0 +1,12 @@
+def createArray(size):
+    return [1 for i in range(size)]
+
+print(createArray(10))
+'''
+Given an integer size, return array of length size filled with 1s.
+
+Example
+
+For size = 4, the output should be
+createArray(size) = [1, 1, 1, 1].
+'''
